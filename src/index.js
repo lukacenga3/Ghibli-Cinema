917c6800d9b35ef33a031b04e75a020e0a9e5892
lukacenga3 <lukacenga@hotmail.com>
2023-12-18 17:31:16 +0100

    updated react-dom

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+ReactDOM.render(
     <React.StrictMode>
         <BrowserRouter>
             <App />
         </BrowserRouter>
-    </React.StrictMode>
+    </React.StrictMode>,
+    document.getElementById("root")
 );
